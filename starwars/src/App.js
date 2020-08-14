b46a984b8ba6755bd8b37b7fb0cfb7bf02d25d86
joel vega <joelvega97@yahoo.com>
2020-08-14 13:43:56 -0400

    wip

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import './App.css';
+import axios from 'axios'
+import CharacterMaker from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +10,32 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [starterpokeData, setStarterPokeData] = useState()
+  const [legendaryPokeData, setLegendaryPokeData] = useState('')
+
+  useEffect(() => {
+    axios('https://pokeapi.co/api/v2/pokemon?limit=9&offset=0')
+      .then(function(res){
+          setStarterPokeData(res.data.results)
+      })
+      .catch(function(err){
+        console.log('OOF')
+      })
+    }, [])
+
+  useEffect(() => {
+    axios('https://pokeapi.co/api/v2/pokemon?limit=8&offset=143')
+    .then(function(res){
+      setLegendaryPokeData(res.data.results)
+    })
+    .catch(function(err){
+      console.log('UHOH')
+    })
+  }, [starterpokeData])
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <CharacterMaker starters = {starterpokeData} legends = {legendaryPokeData} />
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,15 @@
 // Write your Character component here
+
+import React, { useState } from 'react'
+
+export default function CharacterMaker({starters, legends}){
+    const [starterName ,setStarterName] = useState()
+
+
+    return (
+        <div>
+        <h1 className="Header">Pokemon</h1>
+        <p></p>
+        </div>
+    )
+}
